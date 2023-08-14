50ccb1350af0747f64cfda809b52cd17458b09c7
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-08-14 17:05:18 -0300

    Correcao bugs deploy

--- src/database/db.js
+++ src/database/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connection = {
+const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production") connection.ssl = true;
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
-export const db = new Pool(connection)
+export const db = new Pool(configDatabase)
